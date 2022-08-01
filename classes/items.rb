19a42812c4fa42ea2a23e508aa3f5fba21cb13cc
Lekan <jimoridwanlekan@gmail.com>
2022-08-01 12:01:22 +0100

    Add Items classes and associations

---
+++ classes/items.rb
@@ -0,0 +1,40 @@
+class item
+  attr_reader :genre, :author, :label, :Source
+
+  def initialize(publish_date, archived: false)
+    @id = Random.rand(1...1000)
+    @publish_date = Date.new(publish_date)
+    @archived = archived
+  end
+
+  def add_genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author=(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_label=(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
+  def add_source=(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
+
+  def move_to_archive
+    @archived = can_be_archived?
+  end
+
+
+  private
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.today.year) > 10
+  end
+end
