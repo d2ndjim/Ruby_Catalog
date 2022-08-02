3e8234888c578db0c926e1b6443f39d842ef5f95
Bria222 <briantechologies.co.ke@gmail.com>
2022-08-02 16:39:10 +0300

    fix linter

--- game_spec.rb
+++ game_spec.rb
@@ -1,11 +1,10 @@
 require_relative './classes/game'
 describe Game do
-    game = Game.new("shark","yes", "2020/03/15")
-    it "it should create a new instance of Game " do
-    
-        expect(game).to be_instance_of(Game)
-    end
-    it "it should return true" do 
-        expect(game.can_be_archived?).to eq true
-       end
+  game = Game.new('shark', 'yes', '2020/03/15')
+  it 'it should create a new instance of Game ' do
+    expect(game).to be_instance_of(Game)
+  end
+  it 'it should return true' do
+    expect(game.can_be_archived?).to eq true
+  end
 end
