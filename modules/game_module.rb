278bce3ccf2b0eb3848f57af896630484ea5ba03
Bria222 <briantechologies.co.ke@gmail.com>
2022-08-02 13:09:59 +0300

    add game author details to a game

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -17,6 +17,17 @@ module GameModule
       @app.add_game([game_name, last_played_at, multiplayer])
       add_new_author_details
     end
-  
+    
+    def add_new_author_details
+        puts "\n-------------------------------"
+        puts "\nENTER AUTHOR DETAILS\n\n"
+    
+        first_name = capture_input('Input Author Fist Name')
+        last_name = capture_input('Input Author Last Name')
+    
+        @app.add_author([first_name, last_name])
+        puts "\nNew Game and Author Added!"
+        puts "\n-------------------------------"
+      end
     
   end
