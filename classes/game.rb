130350f0da3904d06277a5215d1ffc1047ac0566
Bria222 <briantechologies.co.ke@gmail.com>
2022-08-02 13:23:46 +0300

    add game class

---
+++ classes/game.rb
@@ -0,0 +1,19 @@
+require 'date'
+require_relative './item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at, :game_name
+
+  def initialize(game_name, multiplayer, last_played_at)
+    super(publish_date)
+    @game_name = game_name
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    date = DateTime.parse(@last_played_at).to_date
+    archived = (Date.today.year - date.year) > 2
+    super && archived
+  end
+end
